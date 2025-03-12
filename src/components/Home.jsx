afcdd73a56bda89cccef268f0579f3946cc75d67
mihirmardhav <mihirmardhav2003@gmail.com>
2025-03-12 10:25:57 +0530

    fyjjj

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
       <h1>Welcome to the Home Page</h1>
       <p>This is a simple Home component that you can navigate to using React Router.</p>
       <div className="home-content">
-        <h2>Getting Started</h2>
+        <h2>Started</h2>
         <p>This component is being rendered by the route configuration in App.jsx.</p>
         <p>Feel free to modify this component to suit your needs!</p>
       </div>
